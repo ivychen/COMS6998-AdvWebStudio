0e17223b99a594126e99dc312120164f20c0474c
Ivy Chen <ic2389@columbia.edu>
2018-10-17 23:22:17 -0400

    Updated login

--- Assignments/twitter/app.py
+++ Assignments/twitter/app.py
@@ -33,7 +33,7 @@ class RegistrationForm(Form):
         validators.EqualTo('confirm', message='Passwords must match')
     ])
     confirm = PasswordField('Repeat Password')
-    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    # accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
 
 # Define routes
