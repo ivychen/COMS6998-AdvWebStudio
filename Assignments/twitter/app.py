fd7776192d2641e508f3abe9ad42335fe3ac63cd
Ivy Chen <ic2389@columbia.edu>
2018-10-17 23:56:01 -0400

    updated register

--- Assignments/twitter/app.py
+++ Assignments/twitter/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = '/'
+login_manager.login_view = 'login'
 db = SQLAlchemy(app)
 socketio = SocketIO(app)
 import models
@@ -28,16 +28,17 @@ class UserLoginForm(Form):
 class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
+    password = PasswordField('Password', [
+        validators.DataRequired()
+        # validators.EqualTo('confirm', message='Passwords must match')
     ])
-    confirm = PasswordField('Repeat Password')
+    # confirm = PasswordField('Repeat Password')
     # accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
 
 
 # Define routes
 @app.route('/', methods=['POST', 'GET'])
+@login_required
 def main():
     messages = models.History.query.all()
     return render_template('main.html', messages=messages)
@@ -45,7 +46,7 @@ def main():
 @socketio.on('message')
 def handleMessage(msg):
     print('Message: ' + msg)
-    if current_user.is_authenticated:
+    if current_user.is_authenticated():
         time = datetime.datetime()
         message = models.History(message=msg, timestamp=time.strftime('%Y-%m-%d %H:%M:%S'), sender=current_user.username)
         db.session.add(message)
@@ -57,58 +58,43 @@ def handleMessage(msg):
 # === LOGIN ====
 @login_manager.user_loader
 def load_user(username):
-    return models.User.query.get(username)
-
-def authenticate_user(user):
-    cursor = g.conn.execute("SELECT * FROM Users U WHERE U.username=%s", user.username)
-    data = cursor.fetchone()
-    cursor.close()
-
-    if data[1] == user.password:
-        return True
-    else:
-        return False
+    try:
+        return models.User.query.get(username)
+    except:
+        return None
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated():
         return redirect(url_for('main'))
 
     form = UserLoginForm(request.form)
     error = None
     if request.method == 'POST':
-        user = models.User.query.filter_by(username=request.form['username'].lower()).first()
+        user = models.User.query.filter_by(username=request.form['username'].lower(), password=request.form['password']).first()
         if user:
-            if login_user(user):
-                # app.logger.debug('Logged in user %s', user.username)
-                return redirect(url_for('main'))
-        error = 'Invalid username or password.'
+            login_user(user):
+            print('Logged in user', user.username)
+            return redirect(url_for('main'))
+        else:
+            error = 'Invalid username or password.'
     return render_template('login.html', form=form, error=error)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = models.User(form.username.data, form.password.data, form.email.data)
-        db_session.add(user)
-        # flash('Thanks for registering')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
-def register_user(user):
-    cursor = g.conn.execute("INSERT INTO Users (username, password, email) VALUES (%s, %s, %s)", (user.username, user.password, user.email))
-
-    cursor.close()
-
-def is_registered_user(user):
-    cursor = g.conn.execute("SELECT * FROM Users U WHERE U.username=%s", (user.username, ))
-    data = cursor.fetchone()
-    cursor.close()
-
-    if data:
-        return True
-    else:
-        return False
+    error = None
+    if request.method == 'POST':
+        exist = models.User.query.filter_by(username=request.form['username'].lower()).first()
+        if exist:
+            error = "Username is taken."
+        else:
+            user = models.User(username=request.form['username'].lower(), password=request.form['password'], email=request.form['email'])
+            db.session.add(user)
+            db.session.commit()
+            flash('Thanks for registering')
+            return redirect(url_for('login'))
+    return render_template('register.html', form=form, error=error)
 
 @app.route('/logout')
 @login_required
--- Assignments/twitter/models.py
+++ Assignments/twitter/models.py
@@ -1,4 +1,4 @@
-from app import db
+from app import db, login_manager
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
@@ -6,8 +6,22 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(140))
     email = db.Column(db.Text, unique=True)
 
+    def __init__(self , username ,password , email):
+        self.username = username
+        self.password = password
+        self.email = email
+
     def get_id(self):
         return str(self.username)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
 
     def __repr__(self):
         return "<User {} {}".format(self.username, self.email)
